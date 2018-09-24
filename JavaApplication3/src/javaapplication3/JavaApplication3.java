a3bfa7a2e381d6e3bea1380381356d389b1946ce
Estudiante <Estudiante@lab0711>
2018-09-24 18:31:34 -0500

    #parte jhon

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -17,6 +17,9 @@ public class JavaApplication3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("ESTAMOS DENTRO DE LA PRUEBA...");
+        System.out.println("Practica jhon");
+        
     }
+    
     
 }
